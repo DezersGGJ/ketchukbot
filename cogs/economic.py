8186f01a489e9623854427c08451a6b923a5418e
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-01-26 16:37:49 +0300

    Update economic.py

--- cogs/economic.py
+++ cogs/economic.py
@@ -12,42 +12,42 @@ class Economic(commands.Cog):
 		self.collserver = cluster.ecodb.collserver
 
 	@commands.Cog.listener()
-	async def on_message(self, message):
-		if message.author.bot == False:
-			if message.channel.id == 902855972509327400:
-			umes = self.collection.find_one({"_id": message.author.id})["mes"]
-			self.collection.update_one({"_id": message.author.id}, {"$inc": {"money": 100}})
-			self.collection.update_one({"_id": message.author.id}, {"$inc": {"mes": 1}})
-				if umes == 149:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(903385564781350962)
-					await message.author.add_roles(role_id)
-				elif umes == 299:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(905008758277681153)
-					await message.author.add_roles(role_id)
-				elif umes == 499:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(904708571156066314)
-					await message.author.add_roles(role_id)
-				elif umes == 999:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(904712301255467058)
-					await message.author.add_roles(role_id)
-				elif umes == 1749:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(904714252089188382)
-					await message.author.add_roles(role_id)
-				elif umes == 2999:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(904714499804790786)
-					await message.author.add_roles(role_id)
-				elif umes == 4999:
-					guild = bot.get_guild(message.guild.id)
-					role_id = guild.get_role(904715362715721769)
-					await message.author.add_roles(role_id)
+	async def on_message(message):
+	if message.author.bot == False:
+		if message.channel.id == 902855972509327400:
+			data = collection.find_one({"_id": message.author.id})
+			collection.update_one({"_id": message.author.id}, {"$inc": {"mes": 1}})
+			collection.update_one({"_id": message.author.id}, {"$inc": {"money": 100}})
+			if data["mes"] == 149:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(903385564781350962)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 299:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(905008758277681153)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 499:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(904708571156066314)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 999:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(904712301255467058)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 1749:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(904714252089188382)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 2999:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(904714499804790786)
+				await message.author.add_roles(role_id)
+			elif data["mes"] == 4999:
+				guild = bot.get_guild(message.guild.id)
+				role_id = guild.get_role(904715362715721769)
+				await message.author.add_roles(role_id)
 
-		await bot.process_commands(message)
+	await bot.process_commands(message)
 
 
 def setup(bot):
