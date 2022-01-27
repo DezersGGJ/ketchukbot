efb8e6a46b2a72cd1c0951a03c4c3ce211f4659e
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-01-27 09:31:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -665,21 +665,21 @@ async def removevar(ctx):
 @bot.command()
 @commands.is_owner()
 async def load(ctx, extension):
-    await message.add_reaction('<:yes:903316080456523787>')
+    await ctx.add_reaction('<:yes:903316080456523787>')
     bot.load_extension(f"cogs.{extension}")
 
 
 @bot.command()
 @commands.is_owner()
 async def unload(ctx, extension):
-    await message.add_reaction('<:yes:903316080456523787>')
+    await ctx.add_reaction('<:yes:903316080456523787>')
     bot.unload_extension(f"cogs.{extension}")
 
 
 @bot.command()
 @commands.is_owner()
 async def reload(ctx, extension):
-    await message.add_reaction('<:yes:903316080456523787>')
+    await ctx.add_reaction('<:yes:903316080456523787>')
     bot.unload_extension(f"cogs.{extension}")
     bot.load_extension(f"cogs.{extension}")
 
