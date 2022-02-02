ac563d93973ccfa6297063a483e75271dc424459
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-02-02 15:10:49 +0300

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -300,23 +300,6 @@ class Basic(commands.Cog):
         await ctx.send(embed=embed)
         await member.add_roles(role)
 
-    @addrole.error
-    async def add_role_error(self, ctx, error):
-        if isinstance(error, commands.errors.MissingRequiredArgument):
-            embed = discord.Embed(
-                description = "<:noe:911292323365781515>Аргумент не указан.\n\nИспользование:\n`add-role <user> <role>`",
-                color = 0xff2400
-            )
-            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
-        elif isinstance(error, commands.errors.MemberNotFound):
-            embed = discord.Embed(
-                description = "<:noe:911292323365781515>Пользователь не найден.",
-                color = 0xff2400
-            )
-            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
-
     @commands.command(aliases = ["remove-role"])
     @commands.has_any_role(902849136041295883, 933769903910060153, 902841113734447214)
     async def removerole(self, ctx, member: discord.Member, role: discord.Role):
