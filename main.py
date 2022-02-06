c29f46680b9e7de3170be8fd450ac93c7f91d420
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-02-06 16:03:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ async def baner(ctx):
     name = str(ctx.guild.member_count)
     idraw.text((285, 355), name, font = title, fill = 'white')
     img.save('image.jpg')
-    byte = BytesIO(await img.read())
+    byte = BytesIO(img)
     await ctx.guild.edit(banner = byte)
 
 #owner command
