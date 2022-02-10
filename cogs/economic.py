26786751ad21948dbb17285af6e601c1c378236c
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-02-10 20:45:28 +0200

    Update economic.py

--- cogs/economic.py
+++ cogs/economic.py
@@ -736,7 +736,7 @@ class Economic(commands.Cog):
             try:
                 temp = ctx.guild.get_member(x["_id"])
                 tempmoney = x["money"]
-                embed.add_field(name=f"`{i}.` {temp.name} - {humanize.intcomma(int(tempmoney))}", name="\a", inline=True)
+                embed.add_field(name=f"`{i}.` {temp.name} - {humanize.intcomma(int(tempmoney))}", value="\a", inline=True)
                 i += 1
             except:
                 pass
