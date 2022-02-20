2a0bc3a296f257355bbba7d38ff68f413bf3136b
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-02-20 13:09:10 +0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -20,7 +20,7 @@ class Moderation(commands.Cog):
         role = server.get_role(944888747818901534)
         colors = ['0xFF0000', '0xFFA500', '0xFFFF00', '0x008000', '0x0000FF', '0x800080']
         colot = random.choice(colors)
-        await role.edit(color=colot)
+        await role.edit(color=0xFF0000)
 
     @tasks.loop()
     async def check_mutes(self):
