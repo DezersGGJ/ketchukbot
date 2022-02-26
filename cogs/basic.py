effd4c2f6d18d4fd8fce585c35f8421aae41320a
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-02-26 13:46:01 +0300

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -43,15 +43,12 @@ class Basic(commands.Cog):
         while time.monotonic() < deadline:
             interaction = await self.bot.wait_for("select_option", check=lambda inter: inter.custom_id == "help1")
             res = interaction.values[0]
-            if i < 60:
                 if res == "e1":
                     await interaction.respond(embed=embed1)
                 elif res == "e2":
                     await interaction.respond(embed=embed2)
                 elif res == "e3":
                     await interaction.respond(embed=embed3)
-            else:
-                break
 
     @commands.command()
     async def avatar(self, ctx, *, member: discord.Member=None):
