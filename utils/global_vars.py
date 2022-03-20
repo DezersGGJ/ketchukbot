ad1274accc9ee8fde7603c2f308a2b9271631251
DezersGGJ <96051974+DezersGGJ@users.noreply.github.com>
2022-03-20 09:35:08 +0300

    Create global_vars.py

---
+++ utils/global_vars.py
@@ -0,0 +1,6 @@
+global tracked_players
+global ingame_channels
+#Store player records in <player id> : Player() pairs.
+tracked_players = {}
+#Store games in <channel id> : Game() pairs
+ingame_channels = {}
